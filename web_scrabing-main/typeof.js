29a9fe1d719e2ea5713033678f0a46a378e3b540
Jabina <89601864+Jabina01@users.noreply.github.com>
2022-04-16 14:36:53 +0530

    Delete typeof.js

--- web_scrabing-main/typeof.js
+++
@@ -1,8 +0,0 @@
-// console.log(typeof "Navgurukul"); 
-// console.log(typeof {name: "Shweta", age: 118});
-// console.log(typeof [1, 2, 4]); 
-// console.log(typeof function(){});
-
-
-
-// console.log(typeof ({name: "Shweta", age: 18}));
